1d170b93ebc7976b9cadf60f1bc94443d9a500be
deraj21 <deraj21@gmail.com>
2018-08-20 22:10:58 -0700

    done with css; adding functionality for editing card description and title

--- trello_clone/src/App.js
+++ trello_clone/src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Modal /> */}
-        <Home />
+        <Modal />
+        {/* <Home /> */}
       </div>
       // <div className="App">
       //   <header className="App-header">
--- trello_clone/src/components/Modal/Modal.js
+++ trello_clone/src/components/Modal/Modal.js
@@ -3,7 +3,8 @@ import './Modal.css';
 import { TitleIcon, DescrIcon } from '../Icons/Icons';
 
 const dummyProps = {
-  listName: 'Planning / PreCoding MVP'
+  listName: 'Planning / PreCoding MVP',
+
 };
 
 export default class Modal extends Component {
@@ -12,27 +13,105 @@ export default class Modal extends Component {
 
     this.state = {
       title: 'Create MVP',
-      description: ''
+      description: '',
+      showInput: true,
+      editDescription: false,
+      editTitle: false
     }
+
+    
+  }
+
+  handleChange(key, val) {
+    let obj = {};
+    obj[key] = val;
+    this.setState( obj );
+  }
+
+  edit(key) {
+    let obj = {};
+    obj[key] = true;
+    this.setState( obj );
+  }
+
+  saveTitle() {
+    this.setState({ editTitle: false });
+  }
+
+  cancelEdit() {
+    console.log('cancelEdit');
   }
 
   render(){
 
-    let { title, description } = this.state;
+    let { title, description, showInput, editDescription, editTitle } = this.state;
     let { listName } = dummyProps;
     
     return (
-      <div className="modal">
-        <TitleIcon />
-        <h1>{title}</h1>
-        <p>in list <a>{listName}</a></p>
+      <div className="modal-screen" >
+        <div className="modal">
 
-        <DescrIcon />
-        <h2>Description</h2>
-        <input type="text" placeholder="Add a more detailed description"/>
+          <div className="left">
+            <div className="box">
+              <TitleIcon />
+              <div className="little-box">
+                {
+                  editTitle
+                  ?
+                  <div className="title-input">
+                    <input type="text" value={title} onChange={ e => this.handleChange('title', e.target.value) }/>
+                    <p onClick={ () => this.saveTitle() } >save</p>
+                  </div>
+                  
+                  :
+                  <h1 onClick={ () => this.edit('editTitle') }>{title}</h1>
+                }
+                <p>in list <a href={`#/boards/`}>{listName}</a></p>
+              </div>
+            </div>
+            
+            <div className="box">
+              <DescrIcon />
+              <div className="little-box">
+                <div className="description">
+                  <h2>Description</h2>
+                  <p><a></a></p>
+                </div>
+                {
+                  showInput
+                  ?
+                  <textarea onClick={ () => this.edit('editDescription') } onChange={ e => this.handleChange('description', e.target.value) } type="text" placeholder="Add a more detailed description..."/>
+                  :
+                  <p>{description}</p>
+                }
+                {
+                  editDescription
+                  ?
+                  (
+                    <div className="editing">
+                      <button className="save-btn btn" onClick={ () => {} }>Save Changes</button>
+                      <div className="exit" onClick={ () => {} }>
+                        <div className="one short line"></div>
+                        <div className="two short line"></div>
+                      </div>
+                    </div>
+                  )
+                  :
+                  <p></p>
+                }
+              </div>
+            </div>
+          </div>
 
-        <button>Delete Card</button>
+          <div className="right">
+            <div className="exit">
+              <div className="one long line"></div>
+              <div className="two long line"></div>
+            </div>
+            <button className="btn">Delete Card</button>
+          </div>
 
+        </div>
       </div>
     );
   }
