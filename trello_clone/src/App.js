76dc7dd6016ce37f1bbf7437434dccaf970adaa5
deraj21 <deraj21@gmail.com>
2018-08-21 17:58:34 -0700

    finished Modal Component

--- trello_clone/src/App.js
+++ trello_clone/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './reset.css';
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -9,18 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Home />
         <Modal />
-        {/* <Home /> */}
       </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
     );
   }
 }
--- trello_clone/src/components/Modal/Modal.js
+++ trello_clone/src/components/Modal/Modal.js
@@ -14,12 +14,13 @@ export default class Modal extends Component {
     this.state = {
       title: 'Create MVP',
       description: '',
-      showInput: true,
+      prevDescription: '',
       editDescription: false,
       editTitle: false
     }
 
-    
+    this.handleChange = this.handleChange.bind(this);
+    this.edit = this.edit.bind(this);
   }
 
   handleChange(key, val) {
@@ -31,20 +32,44 @@ export default class Modal extends Component {
   edit(key) {
     let obj = {};
     obj[key] = true;
-    this.setState( obj );
+    if (key === "editDescription") {
+      let desc = this.state.description;
+      this.setState({
+        prevDescription: desc,
+        editDescription: true
+      });
+    } else {
+      this.setState( obj );
+    }
   }
 
-  saveTitle() {
-    this.setState({ editTitle: false });
+  save(key) {
+    let obj = {};
+    obj[key] = false;
+    this.setState( obj );
   }
 
   cancelEdit() {
     console.log('cancelEdit');
+    let desc = this.state.prevDescription;
+    this.setState({
+      editDescription: false,
+      description: desc,
+      prevDescription: ''
+    });
+  }
+
+  delete() {
+    console.log('deleting card');
+  }
+
+  close() {
+    console.log('closing card');
   }
 
   render(){
 
-    let { title, description, showInput, editDescription, editTitle } = this.state;
+    let { title, description, editDescription, editTitle } = this.state;
     let { listName } = dummyProps;
     
     return (
@@ -60,13 +85,12 @@ export default class Modal extends Component {
                   ?
                   <div className="title-input">
                     <input type="text" value={title} onChange={ e => this.handleChange('title', e.target.value) }/>
-                    <p onClick={ () => this.saveTitle() } >save</p>
+                    <p className="text-btn" onClick={ () => this.save('editTitle') } >SAVE</p>
                   </div>
-                  
                   :
                   <h1 onClick={ () => this.edit('editTitle') }>{title}</h1>
                 }
-                <p>in list <a href={`#/boards/`}>{listName}</a></p>
+                <p className="text-btn">in list <a href={`#/boards/`}>{listName}</a></p>
               </div>
             </div>
             
@@ -75,12 +99,12 @@ export default class Modal extends Component {
               <div className="little-box">
                 <div className="description">
                   <h2>Description</h2>
-                  <p><a></a></p>
+                  <p className="text-btn"><a onClick={ () => this.edit('editDescription') } >{ (editDescription || !description) ? '' : 'EDIT' }</a></p>
                 </div>
                 {
-                  showInput
+                  (editDescription || !description)
                   ?
-                  <textarea onClick={ () => this.edit('editDescription') } onChange={ e => this.handleChange('description', e.target.value) } type="text" placeholder="Add a more detailed description..."/>
+                  <textarea onClick={ () => this.edit('editDescription') } onChange={ e => this.handleChange('description', e.target.value) } type="text" placeholder="Add a more detailed description..." value={description}/>
                   :
                   <p>{description}</p>
                 }
@@ -89,8 +113,8 @@ export default class Modal extends Component {
                   ?
                   (
                     <div className="editing">
-                      <button className="save-btn btn" onClick={ () => {} }>Save Changes</button>
-                      <div className="exit" onClick={ () => {} }>
+                      <button className="save-btn btn" onClick={ () => this.save('editDescription') }>Save Changes</button>
+                      <div className="exit" onClick={ () => this.cancelEdit() }>
                         <div className="one short line"></div>
                         <div className="two short line"></div>
                       </div>
@@ -104,11 +128,11 @@ export default class Modal extends Component {
           </div>
 
           <div className="right">
-            <div className="exit">
+            <div onClick={ () => this.close() }  className="exit">
               <div className="one long line"></div>
               <div className="two long line"></div>
             </div>
-            <button className="btn">Delete Card</button>
+            <button onClick={ () => this.delete() } className="btn">Delete Card</button>
           </div>
 
         </div>
