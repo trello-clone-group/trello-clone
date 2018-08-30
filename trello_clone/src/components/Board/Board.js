a97d2eb7e3f691ca4defa4b82533148dc544c91b
Phillip Sun <psun.04@gmail.com>
2018-08-29 20:08:16 -0500

    fixed list and board component compile warnings

--- trello_clone/src/components/Board/Board.js
+++ trello_clone/src/components/Board/Board.js
@@ -1,69 +1,60 @@
-import React,  { Component } from 'react';
-import List from '../List/List'
-import Axios from 'axios';
-import './Board.css'
+import React, { Component } from "react";
+import List from "../List/List";
+import Axios from "axios";
+import "./Board.css";
 //import { Link } from 'react-router-dom';
 
 //import { connect } from 'react-redux'
 
-
 export default class Board extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            listsData: [],
-            boardData: {
-                board_id: null,
-                board_name: '',
-                user_id: null,
-                color: ''
-            }
-        }
-    }
-    
-    componentDidMount(){
-        let { id } = this.props.match.params;
+    this.state = {
+      listsData: [],
+      boardData: {
+        board_id: null,
+        board_name: "",
+        user_id: null,
+        color: ""
+      }
+    };
+  }
 
-        // get boardData
-        Axios.get(`/api/board/${ id }`)
-            .then( response => {
-                this.setState({ boardData: response.data[0] });
-            })
-            .catch( err => err.message );
-        
-        // get listData
-        Axios.get(`/api/lists/`)
-            .then( response => {
-                let lists = response.data.filter(list => list.board_id == id);
-                this.setState({ listsData: lists });
-            })
-            .catch( err => err.message );
-    }
+  componentDidMount() {
+    let { id } = this.props.match.params;
 
-    render(){
+    // get boardData
+    Axios.get(`/api/board/${id}`)
+      .then(response => {
+        this.setState({ boardData: response.data[0] });
+      })
+      .catch(err => err.message);
 
-        let { listsData, boardData } = this.state;
-        let { board_name, color } = boardData;
+    // get listData
+    Axios.get(`/api/lists/`)
+      .then(response => {
+        let lists = response.data.filter(list => list.board_id === id);
+        this.setState({ listsData: lists });
+      })
+      .catch(err => err.message);
+  }
 
-        let lists = listsData.map((list, i) => {
-            return <List key={i} listId={list.list_id} />
-        })
-        
-        return(
-             
-            <div className='boardBackground' style={{backgroundColor: color}}>
-                <div className='boardSubHeader' style={{backgroundColor: color}}>
-                 <h3 className='boardTitle'>{board_name}</h3>
-                 </div>
-                 <div className='listsContainer'>
-                     { lists }
-                </div>
-                    
-            </div>
-        )
-    }
+  render() {
+    let { listsData, boardData } = this.state;
+    let { board_name, color } = boardData;
+
+    let lists = listsData.map((list, i) => {
+      return <List key={i} listId={list.list_id} />;
+    });
+
+    return (
+      <div className="boardBackground" style={{ backgroundColor: color }}>
+        <div className="boardSubHeader" style={{ backgroundColor: color }}>
+          <h3 className="boardTitle">{board_name}</h3>
+        </div>
+        <div className="listsContainer">{lists}</div>
+      </div>
+    );
+  }
 }
-
-
- 
--- trello_clone/src/components/List/List.js
+++ trello_clone/src/components/List/List.js
@@ -1,56 +1,53 @@
-import React,  { Component } from 'react';
-import Axios from 'axios';
-import Card from '../Card/Card';
-import './List.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import Card from "../Card/Card";
+import "./List.css";
 //import { Link } from 'react-router-dom';
 
 //import { connect } from 'react-redux'
 
-
 export default class List extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            listData: {
-                list_id: null,
-                list_name: '',
-                board_id: null
-            },
-            cardsData: []
-        }
-    }
+    this.state = {
+      listData: {
+        list_id: null,
+        list_name: "",
+        board_id: null
+      },
+      cardsData: []
+    };
+  }
 
-    componentDidMount(){
-        let { listId } = this.props;
+  componentDidMount() {
+    let { listId } = this.props;
 
-        Axios.get('/api/lists')
-            .then(response => {
-                let list = response.data.filter(item => item.list_id == listId );
-                this.setState({ listData: list[0] });
-            })
+    Axios.get("/api/lists").then(response => {
+      let list = response.data.filter(item => item.list_id === listId);
+      this.setState({ listData: list[0] });
+    });
 
-        Axios.get(`api/cards/${listId}`)
-            .then( response => {
-                this.setState({ cardsData: response.data });
-            })
-            .catch( err => console.log(err.message));
-    }
+    Axios.get(`api/cards/${listId}`)
+      .then(response => {
+        this.setState({ cardsData: response.data });
+      })
+      .catch(err => console.log(err.message));
+  }
 
-    render(){
-        let { listData, cardsData } = this.state;
-        let { list_name } = listData;
+  render() {
+    let { listData, cardsData } = this.state;
+    let { list_name } = listData;
 
-        let cards = cardsData.map((card, i) => <Card key={i} cardId={card.card_id}/>);
-        
-        return(
-           
-              <div className = 'listBody'>
-                <h3>{list_name}</h3>
-                { cards }
-                </div>
-    
-             
-        )
-    }
+    let cards = cardsData.map((card, i) => (
+      <Card key={i} cardId={card.card_id} />
+    ));
+
+    return (
+      <div className="listBody">
+        <h3>{list_name}</h3>
+        {cards}
+      </div>
+    );
+  }
 }
