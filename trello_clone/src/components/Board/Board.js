e35b6bc18498453ceeb6e69024eb8f03415911e3
Jared Tanner <Deraj21@gmail.com>
2018-09-08 09:38:59 -0700

    styled add-list component

--- trello_clone/src/components/Board/Board.js
+++ trello_clone/src/components/Board/Board.js
@@ -84,6 +84,10 @@ class Board extends Component {
         this.props.updateBoardName(this.state.previousName);
     }
 
+    cancelNewList(){
+        this.setState({ addNewList: false });
+    }
+
     render(){
         let { board_name, lists } = this.props;
         let { color, addNewList, editingBoardName } = this.state;
@@ -120,9 +124,14 @@ class Board extends Component {
                         + Add Another List 
                     </div>
                     :
-                    <div>
+                    <div className="new-list-modal">
                         <input onChange={ e => this.setState({ newListTitle: e.target.value }) } type="text" placeholder="list title"/>
-                        <button onClick={() => this.addList(this.state.newListTitle)} >Add List</button>
+                        <div className="btn-box">
+                            <button onClick={() => this.addList(this.state.newListTitle)} >Add List</button>
+                            <div onClick={() => {this.cancelNewList()}}>
+                                <CancelIcon />
+                            </div>
+                        </div>
                     </div>
                 }
                 
